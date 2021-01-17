44468b7a30ddf83298d3cdc1b49ffc8c2aa34610
Chen Junda <ddadaal@outlook.com>
2021-01-17 09:16:37 +0800

    add infinityscroll at homepage

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box } from "grommet";
+import React, { useRef, useState } from "react";
+import { Box, InfiniteScroll } from "grommet";
 import { WeiboInput } from "src/components/WeiboInput";
 import { NextPage } from "next";
 import { getApi } from "src/apis";
@@ -9,7 +9,9 @@ import { HttpError } from "src/apis/fetch";
 import { UnifiedErrorPage } from "src/components/errors/UnifiedErrorPage";
 import { WeiboListItem } from "src/components/WeiboListItem";
 import { WeiboResult } from "graphweibo-api/weibo/getFollowings";
-import { getCurrentUserInCookie } from "src/stores/UserStore";
+import { getCurrentUserInCookie, UserStore } from "src/stores/UserStore";
+import { useStore } from "simstate";
+import { useHttpRequest } from "src/utils/http";
 
 const api = getApi(weiboApi);
 
@@ -23,20 +25,37 @@ const Home: NextPage<Props> = (props) => {
     return <UnifiedErrorPage error={props.error} />;
   }
 
+  const page = useRef(1);
+  const userStore = useStore(UserStore);
+
+  const [items, setItems] = useState(props.results);
+  const [loading, setLoading] = useState(false);
+
+  const request = useHttpRequest(setLoading);
+
+  const onMore = () => request(async () => {
+    const newPage = page.current + 1;
+    const newWeibos = userStore.user
+      ? await api.getFollowings({ query: { page: newPage } })
+      : await api.getNewWeibos({ query: { page: newPage } });
+    page.current = newPage;
+    setItems((items) => [...items, ...newWeibos.results ]);
+  });
+
   return (
     <Box fill gap="large" alignSelf="center" width={{ max: "large" }}>
       <Box>
         <WeiboInput />
       </Box>
       <Box gap="large">
-        {
-          props.results.map((r) => (
+        <InfiniteScroll items={items} onMore={onMore}>
+          {(r: WeiboResult) => (
             <WeiboListItem
               key={r.weiboId}
               weibo={r}
             />
-          ))
-        }
+          )}
+        </InfiniteScroll>
       </Box>
     </Box>
   );
@@ -46,7 +65,7 @@ Home.getInitialProps = async (ctx) => {
   const user = getCurrentUserInCookie(ctx);
 
   const data = await (
-    user ? api.getFollowings({ query: {} }) : api.getNewWeibos({})
+    user ? api.getFollowings({ query: {} }) : api.getNewWeibos({ query: {} })
   )
     .catch((r: HttpError) => ({ error: r }));
 
