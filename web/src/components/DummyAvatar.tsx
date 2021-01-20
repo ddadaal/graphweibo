509e1a56e4a5ef5ac2781879eef87c3b8351da02
ddadaal <ddadaal@outlook.com>
2021-01-20 16:33:22 +0800

    use local avatar

--- web/src/components/DummyAvatar.tsx
+++ web/src/components/DummyAvatar.tsx
@@ -1,11 +1,11 @@
 import { Avatar, AvatarProps } from "grommet";
+import { User } from "grommet-icons";
 import React from "react";
 
 type Props = AvatarProps;
 
 export const DummyAvatar: React.FC<Props> = (props) => (
-  <Avatar
-    src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80"
-    {...props}
-  />
+  <Avatar background="brand" {...props}>
+    <User color="white" />
+  </Avatar>
 );
